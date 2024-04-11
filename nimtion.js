92ef95087c6e436d8b98e9b4dcdd1220096c97c8
Ta1an7 <166663268+Ta1an7@users.noreply.github.com>
2024-04-12 01:34:26 +0600

    Update nimtion.js

--- nimtion.js
+++ nimtion.js
@@ -14,3 +14,25 @@ function declineInvitation() {
     noButton.style.display = "none";
   }
 }
+
+// Добавлен код для перемещения гифки
+var gifContainer = document.getElementById("gifContainer");
+var isDragging = false;
+var offsetX, offsetY;
+
+gifContainer.addEventListener("mousedown", function(event) {
+  isDragging = true;
+  offsetX = event.clientX - gifContainer.getBoundingClientRect().left;
+  offsetY = event.clientY - gifContainer.getBoundingClientRect().top;
+});
+
+document.addEventListener("mousemove", function(event) {
+  if (isDragging) {
+    gifContainer.style.left = (event.clientX - offsetX) + "px";
+    gifContainer.style.top = (event.clientY - offsetY) + "px";
+  }
+});
+
+document.addEventListener("mouseup", function() {
+  isDragging = false;
+});
